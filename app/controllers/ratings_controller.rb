33c7ee7400ee76a01b470914a9983dd1b8eca2eb
wadror <wadror@wadror-VirtualBox.(none)>
2015-01-22 17:26:49 +0200

    valitallennus

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,19 @@
+class RatingsController < ApplicationController
+  def index
+	@ratings = Rating.all
+  end
+  def new
+	@ratings = Rating.new
+	@beers = Beer.all
+  end
+  def create
+	Ratind.create params.require(:rating).permit(:score, :beer_id)
+	redirect_to ratings_path
+  end
+
+  def destroy
+	rating = Rating.find(params[:id])
+	rating.delete
+	redirect_to ratings_path
+  end
+end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,20 @@
 class Beer < ActiveRecord::Base
+
+	#include RatingAverage
+
 	belongs_to :brewery
+	has_many :ratings
+
+  def average_rating
+	sana = "rating"
+	"Has #{ratings.count} #{sana.pluralize(ratings.count)}, average #{ratings.average(:score)}"
+
+  end
+
+  def panimo
+	a = self.brewery_id
+	b = Brewery.find_by id:a
+	"#{b.name}"
+  end
+
 end
